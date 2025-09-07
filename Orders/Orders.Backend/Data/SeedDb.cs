bf4f834c93e7d0f243fb72e26efe552b7e5e1aa3
Isabel Rua <isabelrua19@gmail.com>
2025-09-06 21:36:46 -0500

    Add DB Seeder

---
+++ Orders/Orders.Backend/Data/SeedDb.cs
@@ -0,0 +1,50 @@
+﻿using Orders.Shared.Entities;
+
+namespace Orders.Backend.Data;
+
+public class SeedDb
+{
+    //Esta clase es un alimentador de datos (seeder).
+    //Esta clase se encarga de inicializar y poblar la base de datos con datos predeterminados.
+
+    //Inyectar la conexión a la base de datos
+    //Para que la inyección permanezca durante todo el ciclo de vida de la app se crea propiedad
+    private readonly DataContext _context; //_context es un campo de la clase estandar
+
+    public SeedDb(DataContext context) //Con lo que esta () se inyecta el contexto de la bd
+    {
+        _context = context;
+    }
+
+    //Método SeedAsync para garantizar que la BD exista y cree registros
+    public async Task SeedAsync()
+    {
+        await _context.Database.EnsureCreatedAsync(); //Asegura que la BD exista, si no existe, se crea la BD.
+
+        //Estos 2 métodos son para garantizar que existan los países y las categorías en la BD.
+        await CheckCountriesAsync();
+        await CheckCategoriesAsync();
+    }
+
+    //Métodos privados para verificar y agregar categorías y países
+    private async Task CheckCategoriesAsync()
+    {
+        if (!_context.Categories.Any()) //Método Any = alguna, se niega condición (!) "Si no hay categorías"
+        {
+            //Adicionar nuevas categorías
+            _context.Categories.Add(new Category { Name = "Calzado" });
+            _context.Categories.Add(new Category { Name = "Tecnología" });
+            await _context.SaveChangesAsync(); //Guardar los cambios
+        }
+    }
+
+    private async Task CheckCountriesAsync()
+    {
+        if (!_context.Countries.Any()) //Método Any = alguna, se niega condición (!) "Si no hay países"
+        {
+            _context.Countries.Add(new Country { Name = "Colombia" });
+            _context.Countries.Add(new Country { Name = "Bolivia" });
+            await _context.SaveChangesAsync(); //Guardar los cambios
+        }
+    }
+}
--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -19,7 +19,10 @@ builder.Services.AddSwaggerGen();
 //"Name=LocaltConnection" es para que tome el string de conexión del appsettings.json
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocaltConnection"));
 
-//Inyección de dependencias
+//Inyectar el servicio de SeedDb de forma "Transient" (transitorio)
+builder.Services.AddTransient<SeedDb>();
+
+//Inyección de dependencias de forma "Scoped" (alcance o ámbito)
 //Antes de hacer el bill se le dice "que me inyecte al Builder.Servicies,
 //adicione de tipo de la unidad de tabajo genérico el repositorio genérico
 builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWork<>));
@@ -29,14 +32,28 @@ builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepositor
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//Luego de crear la app, se inyecata llamando el método SeedData que recibe la app (WebApplication)
+SeedData(app);
 
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-app.Run();
+void SeedData(WebApplication app)
+{
+    //scopedFactory es la forma de llamar las direcciones de los servicios
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    //Con esto se garantiza que cada que se corra el programa se ejecute el método SeedAsync de la clase SeedDb
+    using var scope = scopedFactory!.CreateScope();
+    var service = scope.ServiceProvider.GetService<SeedDb>();
+    service!.SeedAsync().Wait(); //Es .Wait porque se llama un método async desde un método que no es async
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
+}
